65085768c9c079283635aa9bf85a6b3db97b1015
aravinth <aravinth.raja@aline-consulting.com>
2019-10-09 13:21:40 +0530

    updated spark version and cassandra driver version

--- build.sbt
+++ build.sbt
@@ -4,7 +4,7 @@ version := "0.1"
 
 scalaVersion := "2.11.8"
 
-lazy val sparkVersion = "2.1.1"
+lazy val sparkVersion = "2.4.4"
 
 resolvers ++= Seq(
   "apache-snapshots" at "http://repository.apache.org/snapshots/"
@@ -20,7 +20,7 @@ libraryDependencies += "org.apache.spark" %% "spark-core" % sparkVersion
 libraryDependencies += "org.apache.spark" %% "spark-sql" % sparkVersion
 
 //cassandra dependeincies
-libraryDependencies += "com.datastax.spark" %% "spark-cassandra-connector" % "2.3.2"
+libraryDependencies += "com.datastax.spark" %% "spark-cassandra-connector" % "2.4.1"
 libraryDependencies += "com.twitter" % "jsr166e" % "1.1.0"
 
 //memsql dependencies
---
+++ src/main/scala/com/spark/knowledge/test/CassandraTest.scala
@@ -0,0 +1,32 @@
+package com.spark.knowledge.test
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+import  com.spark.knowledge.datastorage.Cassandra
+
+object CassandraTest extends App{
+
+  lazy val sparkConf = new SparkConf()
+    .set("spark.cassandra.connection.host", "192.168.2.19")
+    .set("spark.cassandra.auth.username", "cassandra")
+    .set("spark.cassandra.auth.password", "cassandra")
+    .setAppName("Learn Spark")
+    .setMaster("local")
+    .set("spark.cores.max", "2")
+
+   val sparkSession = SparkSession
+    .builder()
+    .config(sparkConf)
+    .getOrCreate()
+
+  val cassObj = Cassandra.withKeyspace("test")
+
+  println(cassObj.getCassandraVersion)
+
+  val sales_df = sparkSession.read.option("header",true).option("inferSchema",true).csv("/data/big data/Sale.csv")
+  //saving Data to Cassandra
+  cassObj.save(sales_df,"sales")
+  //reading Data from Cassandra
+  val df = cassObj.read("sales")
+    df.show()
+
+}
